014c92effee390485d3e95bf109694a1dd734f1c
moaz basheer <moazbasheer97@gmail.com>
2020-02-13 23:56:59 +0200

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -5,47 +5,47 @@
 
 using namespace std;
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+class MainWindow;
+}
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow: public QMainWindow {
+	Q_OBJECT
 
 public:
-    string RemovedData;
-    vector<string> A;
-    vector<string> B;
-    vector<string> C;
-    vector<pair<char,char>> Move;
-    QPixmap pic;
-    QPixmap pic1;
-    QPixmap pic2;
-    QPixmap pic3;
-    int MoveNumber;
-    int Clicked;
-    void moveRock(char start,char end);
-    void MoveSingleDisk(char start, char finish);
-    void MoveTower(int n, char start, char finish, char temp);
-    void UpdateGUI();
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+	string RemovedData;
+	vector<string> A;
+	vector<string> B;
+	vector<string> C;
+	vector<pair<char, char>> Move;
+	QPixmap pic;
+	QPixmap pic1;
+	QPixmap pic2;
+	QPixmap pic3;
+	int MoveNumber;
+	int Clicked;
+	void moveRock(char start, char end);
+	void MoveSingleDisk(char start, char finish);
+	void MoveTower(int n, char start, char finish, char temp);
+	void UpdateGUI();
+	MainWindow(QWidget *parent = nullptr);
+	~MainWindow();
 
 private slots:
 
+	void on_pushButton_clicked();
 
-    void on_pushButton_clicked();
+	void on_action1_2_triggered();
 
-    void on_action1_2_triggered();
+	void on_action2_2_triggered();
 
-    void on_action2_2_triggered();
+	void on_action3_2_triggered();
 
-    void on_action3_2_triggered();
-
-    void on_action4_triggered();
+	void on_action4_triggered();
 
 private:
-    Ui::MainWindow *ui;
+	Ui::MainWindow *ui;
 };
 
 #endif // MainWindow_H
